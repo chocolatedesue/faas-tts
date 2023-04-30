72773f314274349e27a41ffa29203ca33fea9f4b
chocolatedesue <91049859+chocolatedesue@users.noreply.github.com>
2023-04-30 08:00:38 +0000

    fix: scw

--- func/scw/README.md
+++ func/scw/README.md
@@ -3,3 +3,8 @@ https://www.scaleway.com/en/docs/compute/instances/api-cli/creating-managing-ins
 
 
 only support g2p without tts
+
+# 无法运行包太大 无法上传 
+https://www.scaleway.com/en/docs/serverless/functions/reference-content/functions-limitations/
+cp ./func.zip .serverless/newttsg2p.zip
+sls deploy --package ./
--- func/scw/build.sh
+++ func/scw/build.sh
@@ -8,14 +8,25 @@ function build_package(){
 PYTHON_VERSION=3.9 # or 3.7, 3.8, ...
 docker run --rm -v $(pwd):/home/app/function --workdir /home/app/function rg.fr-par.scw.cloud/scwfunctionsruntimes-public/python-dep:$PYTHON_VERSION python -m pip install --upgrade pip --no-cache-dir && pip install -r requirements.txt --target ./packages --no-cache-dir
 
-cd ./packages
-python -c "import pyopenjtalk; pyopenjtalk._lazy_init()"
-  python -c "import jieba; jieba.initialize()"
-cd -
+# cd ./packages
+# python -c "import pyopenjtalk; pyopenjtalk._lazy_init()"
+#   python -c "import jieba; jieba.initialize()"
+# cd -
 
 }
 
 function deplot(){
+
+find . -name "*.pyc" -exec rm -f {} \;
+find . -name "__pycache__" -exec rm -rf {} 
+
+cd packages
+7z a ../func.zip ./* -r
+cd ../
+7z a func.zip text handlers -r
+
+7z l func.zip
+
 sls deploy --package ./func.zip 
 
 }
